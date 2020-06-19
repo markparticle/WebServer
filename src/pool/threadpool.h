9e2eeaa4479357104cf051d79c35db19080694ef
mark <mark.iii@live.com>
2020-06-19 12:01:02 +0800

    :sparkles: Add ThreadPool

---
+++ src/pool/threadpool.h
@@ -0,0 +1,72 @@
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-15
+ * @copyleft GPL 2.0
+ */ 
+
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <functional>
+
+class ThreadPool {
+
+public:
+    explicit ThreadPool(size_t threadCount): pool_(std::make_shared<Pool>()) {
+            for(size_t i = 0; i < threadCount; i++) {
+                std::thread([pool = pool_] {
+                    std::unique_lock<std::mutex> locker(pool->mtx);
+                    while(true) {
+                        if(!pool->tasks.empty()) {
+                            auto current = std::move(pool->tasks.front());
+                            pool->tasks.pop();
+                            locker.unlock();
+                            current();
+                            locker.lock();
+                        } 
+                        else if(pool->isClosed) break;
+                        else pool->cond.wait(locker);
+                    }
+                }).detach();
+            }
+    }
+
+    ThreadPool() = default;
+
+    ThreadPool(ThreadPool&&) = default;
+    
+    ~ThreadPool() {
+        if(static_cast<bool>(pool_)) {
+            {
+                std::lock_guard<std::mutex> locker(pool_->mtx);
+                pool_->isClosed = true;
+            }
+            pool_->cond.notify_all();
+        }
+    }
+
+    template<class F>
+    void addTask(F&& task) {
+        {
+            std::lock_guard<std::mutex> locker(pool_->mtx);
+            pool_->tasks.emplace(std::forward<F>(task));
+        }
+        pool_->cond.notify_one();
+    }
+    
+private:
+    struct Pool {
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks;
+    };
+    std::shared_ptr<Pool> pool_;
+};
+
+
+#endif //THREADPOOL_H
