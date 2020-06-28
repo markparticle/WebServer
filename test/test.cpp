12d615e409c6f951e2d628a429b423997ba4a413
mark <mark.iii@live.com>
2020-06-28 17:52:23 +0800

    :white_check_mark: test log

--- test/test.cpp
+++ test/test.cpp
@@ -3,30 +3,29 @@
  * @Date         : 2020-06-20
  * @copyleft GPL 2.0
  */ 
-#include "../src/log/log.h"
+#include "../code/log/log.h"
 
 void TestLog() {
     int cnt = 0, level = 0;
-    Log::GetInstance()->init(level, "./test/testlog1", ".log", 5000, 0);
+    Log::Instance()->init(level, "./test/testlog1", ".log", 0);
     for(level = 3; level >= 0; level--) {
-        Log::GetInstance()->setLevel(level);
-        for(int j = 0; j < 1000; j++ ){
+        Log::Instance()->SetLevel(level);
+        for(int j = 0; j < 100000; j++ ){
             for(int i = 0; i < 4; i++) {
-                LOG_TEST(i,"% s============%d 2222222222 ", "Test", cnt++);
+                LOG_BASE(i,"%s============%d 2222222222 ", "Test", cnt++);
             }
         }
     }
     cnt = 0;
-    Log::GetInstance()->init(level, "./test/testlog2", ".log", 5000, 200);
+    Log::Instance()->init(level, "./test/testlog2", ".log", 5000);
     for(level = 0; level < 4; level++) {
-        Log::GetInstance()->setLevel(level);
-        for(int j = 0; j < 1000; j++ ){
+        Log::Instance()->SetLevel(level);
+        for(int j = 0; j < 100000; j++ ){
             for(int i = 0; i < 4; i++) {
-                LOG_TEST(i,"%s 111111111 %d ============= ", "Test", cnt++);
+                LOG_BASE(i,"%s 111111111 %d ============= ", "Test", cnt++);
             }
         }
     }
-
 }
 
 int main() {
