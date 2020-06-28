e1bd71d50acf1b42592451c341f95ee1944aef4a
mark <mark.iii@live.com>
2020-06-28 17:50:49 +0800

    :sparkles: add dynamic buffer

---
+++ code/buffer/buffer.h
@@ -0,0 +1,62 @@
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-26
+ * @copyleft GPL 2.0
+ */ 
+
+#ifndef BUFFER_H
+#define BUFFER_H
+#include <cstring>   //perror
+#include <iostream>
+#include <unistd.h>  // write
+#include <sys/uio.h> //readv
+#include <vector> //readv
+#include <atomic>
+#include <assert.h>
+
+
+class Buffer {
+public:
+    Buffer();
+    ~Buffer() = default;
+
+    size_t WritableBytes() const;       
+    size_t ReadableBytes() const ;
+    size_t prependableBytes() const;
+
+    const char* Peek() const;
+    char* Peek();
+    void EnsureWriteable(size_t len);
+    void HasWritten(size_t len);
+
+    void Retrieve(size_t len);
+    void RetrieveUntil(const char* end);
+
+    void RetrieveAll() ;
+    std::string RetrieveAllToStr();
+
+    const char* BeginWriteConst() const;
+    char* BeginWrite();
+
+    void Append(const std::string& str);
+    void Append(const char* data, size_t len);
+    void Append(const void* data, size_t len);
+    void Append(const Buffer& buff);
+
+    ssize_t ReadFd(int fd, int* Errno);
+    ssize_t WriteFd(int fd, int* Errno);
+
+private:
+    const size_t MAX_BUFF_SIZE = 65536;
+    const size_t INIT_BUFF_SIZE = 1024;
+
+    char* BeginPtr_();
+    const char* BeginPtr_() const;
+    void MakeSpace_(size_t len);
+
+    std::vector<char> buffer_;
+    std::atomic<std::size_t> readPos_;
+    std::atomic<std::size_t> writePos_;
+};
+
+#endif //BUFFER_H
