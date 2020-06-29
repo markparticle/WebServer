b3b132633affee5f46e81cf289357d65a5861757
mark <mark.iii@live.com>
2020-06-29 12:18:21 +0800

    :white_check_mark: add threadpool and log test

--- test/test.cpp
+++ test/test.cpp
@@ -4,30 +4,47 @@
  * @copyleft GPL 2.0
  */ 
 #include "../code/log/log.h"
+#include "../code/pool/threadpool.h"
 
 void TestLog() {
     int cnt = 0, level = 0;
-    Log::Instance()->init(level, "./log//testlog1", ".log", 0);
+    Log::Instance()->init(level, "./testlog1", ".log", 0);
     for(level = 3; level >= 0; level--) {
         Log::Instance()->SetLevel(level);
-        for(int j = 0; j < 100000; j++ ){
-            for(int i = 0; i < 4; i++) {
-                LOG_BASE(i,"%s============%d 2222222222 ", "Test", cnt++);
-            }
-        }
-    }
-    cnt = 0;
-    Log::Instance()->init(level, "./log/testlog2", ".log", 5000);
-    for(level = 0; level < 4; level++) {
-        Log::Instance()->SetLevel(level);
-        for(int j = 0; j < 100000; j++ ){
+        for(int j = 0; j < 10000; j++ ){
             for(int i = 0; i < 4; i++) {
                 LOG_BASE(i,"%s 111111111 %d ============= ", "Test", cnt++);
             }
         }
     }
+    cnt = 0;
+    Log::Instance()->init(level, "./testlog2", ".log", 5000);
+    for(level = 0; level < 4; level++) {
+        Log::Instance()->SetLevel(level);
+        for(int j = 0; j < 10000; j++ ){
+            for(int i = 0; i < 4; i++) {
+                LOG_BASE(i,"%s 222222222 %d ============= ", "Test", cnt++);
+            }
+        }
+    }
+}
+
+void ThreadLogTask(int i, int cnt) {
+    for(int j = 0; j < 10000; j++ ){
+        LOG_BASE(i,"PID:[%04d]======= %05d ========= ", gettid(), cnt++);
+    }
+}
+
+void TestThreadPool() {
+    Log::Instance()->init(0, "./testThreadpool", ".log", 5000);
+    ThreadPool threadpool(6);
+    for(int i = 0; i < 18; i++) {
+        threadpool.addTask(std::bind(ThreadLogTask, i % 4, i * 10000));
+    }
+    getchar();
 }
 
 int main() {
     TestLog();
+    TestThreadPool();
 }
